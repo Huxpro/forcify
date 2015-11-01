2e7356f3c7f33617f270f5b3cc03485cdfdb52e0
黄玄 <huxpro@gmail.com>
2015-11-02 00:09:08 +0800

    core done

--- demo/3dtouch.js
+++ demo/3dtouch.js
@@ -10,19 +10,19 @@ import Forcify from '../src/forcify';
 // Hux Forcer.js
 
 var qf = new Forcify(document.querySelector("#forceMe"));
-qf.on('forcechange', function(e){
+qf.on('force', function(e){
     //console.log(e.force)
     renderElement(e.force, element)
 });
 
 var qf2 = new Forcify(document.querySelector("#forceMe2"));
-qf2.on('forcechange', function(e){
+qf2.on('force', function(e){
     //console.log(e.force)
     renderElement(e.force, element2)
 });
 
 function renderElement(forceValue, _element) {
     _element.style.webkitTransform = 'translateX(-50%) translateY(-50%) scale(' + (1 + forceValue * 1.5) + ')';
-    background.style.webkitFilter = 'blur(' + forceValue * 30 + 'px)';
+    //background.style.webkitFilter = 'blur(' + forceValue * 30 + 'px)';
     forceValueOutput.innerHTML = 'Force: ' + forceValue.toFixed(4);
 }
